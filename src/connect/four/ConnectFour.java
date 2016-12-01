44620d2a68ef459bb86243a9ae1d490de94c375c
jkfitzpatrick <justin.fitzpatrick82@gmail.com>
2016-12-01 17:35:41 -0600

    Justin Edits, Dec 1st, 5:35PM
    Implemented fix for known bug

--- src/connect/four/ConnectFour.java
+++ src/connect/four/ConnectFour.java
@@ -18,12 +18,22 @@ public class ConnectFour {
 	ConsolePlayer p1 = new ConsolePlayer(stdin.readLine());
 	System.out.print("How many players (1/2)?");
 	Player p2;
-	if (stdin.readLine().equals("1")) {
-	    p2 = new ComputerPlayer();
-	} else {
+	String num = stdin.readLine();
+	//Edited to only accept user entry of "1" or "2"
+	while(!num.equals("1") && !num.equals("2"))
+		{
+		System.out.println("Please enter either \"1\" or \"2\"");
+		num = stdin.readLine();
+		}
+	if (num.equals("1")) 
+		{
+	    	p2 = new ComputerPlayer();
+		} 
+	else 
+		{
 	    System.out.print("Other player's name: ");
 	    p2 = new ConsolePlayer(stdin.readLine());
-	}
+		}
 	Game game = new Game(new Player[] {p1, p2}, new Board(7, 6), 4);
 	game.registerListener(p1);
 	if (p2 instanceof ScoreChart.Listener) game.registerListener((ScoreChart.Listener)p2);
